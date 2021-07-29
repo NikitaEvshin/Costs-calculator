af019bc67eab75f509f5c2fd38809ca3a8444a69
Your Name <you@example.com>
2021-07-29 17:33:11 +0300

    NewCalc

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double summaelect = (double.Parse(Electnew.Text) - double.Parse(Electold.Text)) *
+                double.Parse(Electtariff.Text);
+
+            double summacoldwater = (double.Parse(Coldwaternew.Text)
+                                     - double.Parse(Coldwaterold.Text)) * double.Parse(Colfwatertariff.Text);
+
+            double suumahotwater = (double.Parse(Hotwaternew.Text) - double.Parse(Hotwaterold.Text)) *
+                                    double.Parse(Hotwatertariff.Text);
+
+            double summa = summaelect + summacoldwater + suumahotwater;
+            Summaresult.Text = summa.ToString("F2");
+        }
+    }
+}
